25d263eba5fa8fe8b075bf3189a6aaf140f1abf9
enriquejdd <enriquejdd1999@gmail.com>
2021-02-03 14:26:58 +0100

    Añadimos atributos y constructores

--- src/main/java/enrique/t6aenriquediaz/Animal.java
+++ src/main/java/enrique/t6aenriquediaz/Animal.java
@@ -10,5 +10,27 @@ package enrique.t6aenriquediaz;
  * @author Enrique
  */
 public class Animal {
+    String tamanio;
+    String tipoAnimal;
+    boolean esSigiloso;
+    int subgénero;
+
+    public Animal(String tamanio, String tipoAnimal, boolean esSigiloso, int subgénero) {
+        this.tamanio = tamanio;
+        this.tipoAnimal = tipoAnimal;
+        this.esSigiloso = esSigiloso;
+        this.subgénero = subgénero;
+    }    
+
+    public Animal() {
+        this.tamanio = "Pequeño";
+        this.tipoAnimal = "Felino";
+        this.esSigiloso = true;
+        this.subgénero = 4;
+    }
+    
+    
+    
+    
     
 }
