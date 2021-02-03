a2c81e898847b8dab352ceeb03b0bc1098abb327
enriquejdd <enriquejdd1999@gmail.com>
2021-02-03 14:34:35 +0100

    Añadimos getters, setters, toString, equal y hascode y añadimos 3 métodos

--- src/main/java/enrique/t6aenriquediaz/Animal.java
+++ src/main/java/enrique/t6aenriquediaz/Animal.java
@@ -5,32 +5,137 @@
  */
 package enrique.t6aenriquediaz;
 
+import java.util.Objects;
+
 /**
  *
  * @author Enrique
  */
 public class Animal {
-    String tamanio;
-    String tipoAnimal;
-    boolean esSigiloso;
-    int subgénero;
 
-    public Animal(String tamanio, String tipoAnimal, boolean esSigiloso, int subgénero) {
+    private String tamanio;
+    private String tipoAnimal;
+    private boolean esSigiloso;
+    private int subgénero;
+    private int estadoAnimal; // 1- Cazando, 2- Descansando, 3-Jugando
+
+    public Animal(String tamanio, String tipoAnimal, boolean esSigiloso, int subgénero, int estadoAnimal) {
         this.tamanio = tamanio;
         this.tipoAnimal = tipoAnimal;
         this.esSigiloso = esSigiloso;
         this.subgénero = subgénero;
-    }    
+        this.estadoAnimal = estadoAnimal;
+    }
 
     public Animal() {
         this.tamanio = "Pequeño";
         this.tipoAnimal = "Felino";
         this.esSigiloso = true;
         this.subgénero = 4;
+        this.estadoAnimal = 2;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public String getTipoAnimal() {
+        return tipoAnimal;
+    }
+
+    public void setTipoAnimal(String tipoAnimal) {
+        this.tipoAnimal = tipoAnimal;
+    }
+
+    public boolean isEsSigiloso() {
+        return esSigiloso;
+    }
+
+    public void setEsSigiloso(boolean esSigiloso) {
+        this.esSigiloso = esSigiloso;
+    }
+
+    public int getSubgénero() {
+        return subgénero;
+    }
+
+    public void setSubgénero(int subgénero) {
+        this.subgénero = subgénero;
+    }
+
+    public int getEstadoAnimal() {
+        return estadoAnimal;
+    }
+
+    public void setEstadoAnimal(int estadoAnimal) {
+        this.estadoAnimal = estadoAnimal;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "tamanio=" + tamanio + ", tipoAnimal=" + tipoAnimal + ", esSigiloso=" + esSigiloso + ", subg\u00e9nero=" + subgénero + ", estadoAnimal=" + estadoAnimal + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.tamanio);
+        hash = 71 * hash + Objects.hashCode(this.tipoAnimal);
+        hash = 71 * hash + (this.esSigiloso ? 1 : 0);
+        hash = 71 * hash + this.subgénero;
+        hash = 71 * hash + this.estadoAnimal;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Animal other = (Animal) obj;
+        if (this.esSigiloso != other.esSigiloso) {
+            return false;
+        }
+        if (this.subgénero != other.subgénero) {
+            return false;
+        }
+        if (this.estadoAnimal != other.estadoAnimal) {
+            return false;
+        }
+        if (!Objects.equals(this.tamanio, other.tamanio)) {
+            return false;
+        }
+        if (!Objects.equals(this.tipoAnimal, other.tipoAnimal)) {
+            return false;
+        }
+        return true;
     }
     
+    public void cazar(Animal e){
+        System.out.println("El animal se encuentra cazando");
+        e.setEstadoAnimal(1);
+    }
     
+    public void descansar(Animal e){
+        System.out.println("El animal se encuentra descansando");
+        e.setEstadoAnimal(2);
+    }
     
+    public void descansar(Animal e, int tiempo){
+        e.setEstadoAnimal(2);
+        System.out.println("El animal va ha descansar durante " + tiempo + " horas");        
+    }
     
-    
+
+
 }
