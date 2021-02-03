3ee90cd7d2eb4086f0f20293b8b8f972acbfd9b2
enriquejdd <enriquejdd1999@gmail.com>
2021-02-03 17:42:15 +0100

    Insertamos getters, setters, toStrign formateado, equal y hashCode

--- src/main/java/enrique/t6aenriquediaz/FelisSilvestrisCatus.java
+++ src/main/java/enrique/t6aenriquediaz/FelisSilvestrisCatus.java
@@ -5,14 +5,17 @@
  */
 package enrique.t6aenriquediaz;
 
+import java.util.Objects;
+
 /**
  *
  * @author Enrique
  */
 public class FelisSilvestrisCatus extends Felino {
-    String color;
-    int peso;
-    boolean esCallejero;
+
+    private String color;
+    private int peso;
+    private boolean esCallejero;
 
     public FelisSilvestrisCatus(String color, int peso, boolean esCallejero, String origen, boolean esNocturno, int tamanio, String tipoAnimal, boolean esSigiloso, int subgénero, int estadoAnimal) {
         super(origen, esNocturno, tamanio, tipoAnimal, esSigiloso, subgénero, estadoAnimal);
@@ -27,6 +30,67 @@ public class FelisSilvestrisCatus extends Felino {
         this.peso = 3750;
         this.esCallejero = false;
     }
-    
-    
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public boolean isEsCallejero() {
+        return esCallejero;
+    }
+
+    public void setEsCallejero(boolean esCallejero) {
+        this.esCallejero = esCallejero;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "FelisSilvestrisCatus{" + "color=" + color + ", peso=" + peso + ", esCallejero=" + esCallejero + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.color);
+        hash = 67 * hash + this.peso;
+        hash = 67 * hash + (this.esCallejero ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FelisSilvestrisCatus other = (FelisSilvestrisCatus) obj;
+        if (this.peso != other.peso) {
+            return false;
+        }
+        if (this.esCallejero != other.esCallejero) {
+            return false;
+        }
+        if (!Objects.equals(this.color, other.color)) {
+            return false;
+        }
+        return true;
+    }
+
 }
